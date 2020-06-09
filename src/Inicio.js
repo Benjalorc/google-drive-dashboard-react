ea542d17ed2373410ca6737b3d40c35ccea68f0b
Benjalorc <benjalorc@gmail.com>
2020-06-09 14:48:28 -0400

    Componente base Inicio sin el carousel de imagenes

---
+++ src/Inicio.js
@@ -0,0 +1,131 @@
+import React {useState} from 'react';
+import './Inicio.css';
+
+renderButton(){
+
+  	setLoading(true);
+
+  	//Inicializa un boton pre-configurado para permitir
+  	//El inicio de sesion
+
+  	let onSignIn = (googleUser)=> {
+
+  		gUser.current = googleUser;
+  		username.current = googleUser.getBasicProfile().getName();
+  		imgUrl.current = googleUser.getBasicProfile().getImageUrl();
+		setLogged(true);
+		setLoading(false);
+	}
+
+	let onFailure = (err)=>{ 
+		console.log(err);
+		setLoading(false);
+	}
+	
+  	gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 220,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'light',
+        'onsuccess': onSignIn,
+        'onfailure': onFailure
+    });
+
+    setTimeout(() =>{
+
+		let element = document.querySelector("#my-signin2 > div");
+		element.addEventListener("click", ()=>{
+			setLoading(true);
+		});
+    },1000);
+    setLoading(false);
+}
+
+cambiarUsuario(){
+	let element = document.querySelector("#my-signin2 > div");
+	element.click()
+}
+
+function Inicio() {
+
+  let [isLogged, setLogged] = useState(false);
+  let [loading, setLoading] = useState(false);
+
+  if(isLogged){
+
+	  return (
+		<div className="row login">
+			<div className="col-12 col-md-3"></div>
+			<div className="col-12 col-md-6">
+
+
+				<div className="card bg-white text-center box-shadow-1">
+
+					<div className="card-header">
+						<span className="golden"> <i className="fa fa-key" aria-hidden="true"></i> </span>Acceso Autorizado
+					</div>
+					
+					<div className="card-body">
+						<div className="row">
+							<div className="col">
+								<div id="my-signin2"></div>
+							</div>
+						</div>
+
+						<div className="row">
+
+							<div className="col-3 col-md-4"></div>
+							<div className="col-6 col-md-4 profilePic">
+								<img className="card-img-top profilePic" (click)="cambiarUsuario()" [src]="imgUrl">
+								<span className="overlay-switch" (click)="cambiarUsuario()"><i className="fa fa-exchange" aria-hidden="true"></i></span>
+							</div>
+							<div className="col-3 col-md-4"></div>
+							<div className="col-12">
+								<h5 className="card-title">Bienvenido(a) {username}!</h5>
+								<p className="card-text">Ya puede ingresar a su dashboard haciendo click en el botón a continuación.</p>
+								<a className="btn btn-outline-success" (click)="goDashboard()">Continuar</a>
+							</div>
+						</div>
+					</div>
+					
+					<div className="card-footer text-muted">
+						<span className="wrongUserLink" (click)="cambiarUsuario()"> ¿No eres {username}? </span> 
+					</div>
+				
+				</div>
+			<div className="col-12 col-md-3"></div>
+		</div>
+	  );
+
+  }
+  else{
+
+	  return (
+		<div className="row login">
+			<div className="col-12 col-md-3"></div>
+			<div className="col-12 col-md-6">
+
+				<div className="card bg-white text-center box-shadow-1">
+
+					<div className="card-header">
+
+					</div>
+
+					<div className="card-body">
+						<div className="row">
+							<div className="col">
+								<div id="my-signin2"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="col-12 col-md-3"></div>
+		</div>
+	  );
+  }
+
+}
+
+export default Inicio;
