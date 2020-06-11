8d5ee92a7c8be8360ae9b25ee656dbb8e789886a
Benjalorc <benjalorc@gmail.com>
2020-06-10 20:54:17 -0400

    Componente mostrando un conteo clasificado de los archivos en drive

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -11,6 +11,70 @@ import './Dashboard.css';
 import { Chart } from 'chart.js';
 import { TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
 
+
+function GeneralOverview(){
+
+	const files = useSelector(state => state.files.list);
+
+	return(
+
+		<div className="row spacing-1">
+		    <div className="col-12 col-sm-6 col-md-3">
+
+		        <div className="card text-center border-info bg-light">
+		            <a href="https://docs.google.com/documents/" target="_blank"><img className="card-img-top link" src="images/docs.png" alt="Card image cap" /></a>
+		            <div className="card-body">
+		                <p>Documentos</p>
+		            </div>
+		            <div className="card-footer border-info text-muted">
+		                <h2>{files.docs.length}</h2>
+		            </div>
+		        </div>
+
+		    </div>
+		    <div className="col-12 col-sm-6 col-md-3">
+
+		        <div className="card text-center border-success bg-light">
+		            <a href="https://docs.google.com/spreadsheets/"><img className="card-img-top link" src="images/spreadsheets.png" alt="Card image cap" /></a>
+		            <div className="card-body">
+		                <p>Hojas de Cálculo</p>
+		            </div>
+		            <div className="card-footer border-success text-muted">
+		                <h2>{files.sheets.length}</h2>
+		            </div>
+		        </div>
+
+		    </div>
+		    <div className="col-12 col-sm-6 col-md-3">
+
+		        <div className="card text-center border-warning bg-light">
+		            <a href="https://docs.google.com/presentation/" target="_blank"><img className="card-img-top link" src="images/slides.png" alt="Card image cap" /></a>
+		            <div className="card-body">
+		                <p>Presentaciones</p>
+		            </div>
+		            <div className="card-footer border-warning text-muted">
+		                <h2>{files.presentations.length}</h2>
+		            </div>
+		        </div>
+
+		    </div>
+		    <div className="col-12 col-sm-6 col-md-3">
+
+		        <div className="card text-center border-danger bg-light">
+		            <a href="https://drive.google.com/" target="_blank"><img className="card-img-top link" src="images/drawings.png" alt="Card image cap" /></a>
+		            <div className="card-body">
+		                <p>Drawings</p>
+		            </div>
+		            <div className="card-footer border-danger text-muted">
+		                <h2>{files.drawings.length}</h2>
+		            </div>
+		        </div>
+
+		    </div>
+		</div>
+	)
+}
+
 function FilesTable({files, titulo, short = true}){
 
 
@@ -449,6 +513,7 @@ function Dashboard(){
 
 	return (
 		<React.Fragment>
+			<GeneralOverview />
 			<StorageUsage />
 			<RecentChanges />
 			<FilesCards doneLoading={()=> spinOff() } />
