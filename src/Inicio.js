918787ec579bae55011d0e483e1c4d3fbe398d99
Benjalorc <benjalorc@gmail.com>
2020-06-11 01:58:58 -0400

    Carousel de imágenes en Inicio Animación de carga y bloqueo de interacción con backdrop en Inicio

--- src/Inicio.js
+++ src/Inicio.js
@@ -1,9 +1,110 @@
-import React, { createRef, useState, useEffect } from 'react';
+import React, { createRef, useState } from 'react';
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "./redux/actions/index";
-import store from "./redux/store/index";
 import './Inicio.css';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+
+const items = [
+  {
+    src: "images/slide-1-transparent.png",
+    altText: 'Slide 1',
+    caption: 'Acceso fácil a documentos'
+  },
+  {
+    src: "images/slide-2-transparent.png",
+    altText: 'Slide 2',
+    caption: 'Prácticas estadísticas'
+  },
+  {
+    src: "images/slide-3-transparent.png",
+    altText: 'Slide 3',
+    caption: 'Haga click para comenzar!'
+  }
+];
+
+function CarouselPromo(){
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} />
+        <CarouselCaption captionHeader={item.caption} />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+    >
+      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+      {slides}
+      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+    </Carousel>
+  );
+}
+
+function UserRow({user, cambiarUsuario}){
+	if(!user || !user.isLogged) return null
+	return(
+		<div className="row">
+
+			<div className="col-3 col-md-4"></div>
+			<div className="col-6 col-md-4 profilePic">
+				<img className="card-img-top profilePic" alt="user" src={user.imgUrl} onClick={cambiarUsuario} />
+				<span className="overlay-switch" onClick={cambiarUsuario}><i className="fa fa-exchange" aria-hidden="true"></i></span>
+			</div>
+			<div className="col-3 col-md-4"></div>
+			<div className="col-12">
+				<h5 className="card-title">Bienvenido(a) {user.username}!</h5>
+				<p className="card-text">Ya puede ingresar a su dashboard haciendo click en el botón a continuación.</p>
+				<Link to="/dashboard" className="btn btn-outline-success">Continuar</Link>
+			</div>
+		</div>
+	)
+}
+
+function UserFoot({user, cambiarUsuario}){
+	if(!user || !user.isLogged) return null
+	return(
+		<div className="card-footer text-muted">
+			<span className="wrongUserLink" onClick={cambiarUsuario}> ¿No eres {user.username}? </span>
+		</div>
+	)
+}
 
 function Inicio(){
 
@@ -14,18 +115,18 @@ function Inicio(){
 
   const user = useSelector(state => state.user);
 
-function waitForGapi(){
-	return new Promise((resolve, reject)=>{
-		let interval = setInterval(check, 1000);
-		function check(){
-			if(window.gapi){
-				gapi.current = window.gapi;
-				clearInterval(interval);
-				resolve(true);
+	function waitForGapi(){
+		return new Promise((resolve, reject)=>{
+			let interval = setInterval(check, 1000);
+			function check(){
+				if(window.gapi){
+					gapi.current = window.gapi;
+					clearInterval(interval);
+					resolve(true);
+				}
 			}
-		}
-	});
-}
+		});
+	}
 
 function renderButton(){
 
@@ -46,11 +147,11 @@ function renderButton(){
 		setLoading(false);
 	}
 
-	let onFailure = (err)=>{ 
+	let onFailure = (err)=>{
 		console.log(err);
 		setLoading(false);
 	}
-	
+
 	gapi.current.signin2.render('my-signin2', {
         'scope': 'profile email',
         'width': 220,
@@ -78,33 +179,33 @@ function renderButton(){
     addEvent();
 
 	setRenderSent(true);
-    //setLoading(false);
+    setLoading(false);
 }
 
-function cambiarUsuario(){
-	let element = document.querySelector("#my-signin2 > div");
-	element.click()
-}
+	function cambiarUsuario(){
+		let element = document.querySelector("#my-signin2 > div");
+		element.click()
+	}
 
-if(!gapi.current){
-	waitForGapi().then(()=> renderButton());
-}
-else{
-	renderButton();
-}
+	if(!gapi.current){
+		waitForGapi().then(()=> renderButton());
+	}
+	else{
+		renderButton();
+	}
 
-  if(user && user.isLogged){
-
-	  return (
+	return (
 		<div className="row login">
 			<div className="col-12 col-md-3"></div>
 			<div className="col-12 col-md-6">
 
-
 				<div className="card bg-white text-center box-shadow-1">
 
 					<div className="card-header">
-						<span className="golden"> <i className="fa fa-key" aria-hidden="true"></i> </span>Acceso Autorizado
+						<CarouselPromo />
+						{
+							user.isLogged ? <span><span className="golden"> <i className="fa fa-key" aria-hidden="true"></i> </span>Acceso Autorizado</span> : null
+						}
 					</div>
 					
 					<div className="card-body">
@@ -113,60 +214,18 @@ else{
 								<div id="my-signin2"></div>
 							</div>
 						</div>
+						<UserRow user={user} cambiarUsuario={cambiarUsuario} />
 
-						<div className="row">
+					</div>
 
-							<div className="col-3 col-md-4"></div>
-							<div className="col-6 col-md-4 profilePic">
-								<img className="card-img-top profilePic" alt="user" src={user.imgUrl} onClick={()=> cambiarUsuario() } />
-								<span className="overlay-switch" onClick={()=> cambiarUsuario() }><i className="fa fa-exchange" aria-hidden="true"></i></span>
-							</div>
-							<div className="col-3 col-md-4"></div>
-							<div className="col-12">
-								<h5 className="card-title">Bienvenido(a) {user.username}!</h5>
-								<p className="card-text">Ya puede ingresar a su dashboard haciendo click en el botón a continuación.</p>
-								<Link to="/dashboard" className="btn btn-outline-success">Continuar</Link>
-							</div>
-						</div>
-					</div>
-					
-					<div className="card-footer text-muted">
-						<span className="wrongUserLink" onClick={()=> cambiarUsuario() }> ¿No eres {user.username}? </span>
-					</div>
-				
+					<UserFoot user={user} cambiarUsuario={cambiarUsuario} />
+
+					{loading ? <div className="backdropLogin"><i className="fa fa-spinner fa-spin"></i></div> : null}
 				</div>
 			</div>
 			<div className="col-12 col-md-3"></div>
 		</div>
-	  );
-
-  }
-  else{
-
-	  return (
-		<div className="row login">
-			<div className="col-12 col-md-3"></div>
-			<div className="col-12 col-md-6">
-
-				<div className="card bg-white text-center box-shadow-1">
-
-					<div className="card-header">
-
-					</div>
-
-					<div className="card-body">
-						<div className="row">
-							<div className="col">
-								<div id="my-signin2"></div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="col-12 col-md-3"></div>
-		</div>
-	  );
-  }
+	);
 
 }
 
